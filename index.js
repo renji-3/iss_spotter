585034f6657f14bbcae62a5f31019684d9cd1653
Ren Guidolin <8.renji@gmail.com>
2021-12-03 00:29:10 -0500

    2

--- index.js
+++ index.js
@@ -23,13 +23,3 @@ const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 
 
-const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
-
-fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-
-  console.log('It worked! Returned flyover times:' , passTimes);
-});
--- iss.js
+++ iss.js
@@ -62,22 +62,7 @@ const fetchCoordsByIP = function(ip, callback) {
 };
 
 const fetchISSFlyOverTimes = function(coords, callback) {
-  const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
 
-  request(url, (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
-      return;
-    }
-
-    const passes = JSON.parse(body).response;
-    callback(null, passes);
-  });
 };
 
 
